dc565eabf79ffd388d4c882c2dfc945d996950f8
Cory House <housecor@gmail.com>
2019-07-29 08:27:12 -0500

    Delete functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+// 2 ways to declare React components:
+// 1. Class
+// 2. Function <- Prefer this. It's simpler. Easier to use
 
 function App() {
+  // Declare state using the useState hook.
+  const [courses, setCourses] = useState([
+    { id: 1, title: "JS Fundamentals", author: "Cory House" },
+    { id: 2, title: "Advanced React", author: "Cory House" }
+  ]);
+
+  function deleteCourse(id) {
+    const updatedCourses = courses.filter(course => course.id !== id);
+    setCourses(updatedCourses); // Updating state will cause React to re-render
+  }
+
+  function renderCourses() {
+    if (courses.length === 0) return <p> No courses.</p>;
+
+    return (
+      <ul>
+        {courses.map(course => (
+          <li>
+            <button onClick={() => deleteCourse(course.id)}>Delete</button>{" "}
+            {course.title}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Courses</h1>
+      {renderCourses()}
+    </>
   );
 }
 
