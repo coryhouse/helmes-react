33fadfcd26feea38d98296c316cee715cc7afeca
Cory House <housecor@gmail.com>
2019-07-31 06:47:31 -0500

    Add 404 page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import Home from "./Home";
 import Courses from "./Courses";
-import { Route } from "react-router-dom";
+import PageNotFound from "./PageNotFound";
+import { Route, Switch } from "react-router-dom";
 
 class App extends React.Component {
   render() {
     return (
       <>
         Nav
-        <Route path="/" component={Home} />
-        <Route path="/courses" component={Courses} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/courses" component={Courses} />
+          <Route component={PageNotFound} />
+        </Switch>
       </>
     );
   }
